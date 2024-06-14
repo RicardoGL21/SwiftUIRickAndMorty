7913067fe324210d480885bdad50f70a0ff7b1a7
Ricardo Garcia <ricardogarcia.lopez@hotmail.com>
2024-06-14 09:23:28 -0600

    Initial Commit

---
+++ PokeApiSwiftUI/PokeApiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiSwiftUIApp.swift
+//  PokeApiSwiftUI
+//
+//  Created by Ricardo Garcia on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
